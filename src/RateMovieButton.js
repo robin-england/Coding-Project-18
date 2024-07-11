42d70193545e6d602f1f198f9210f6fbc72a9248
robin-england <robin.england94@googlemail.com>
2024-07-11 17:04:55 -0400

    Implement RateMovieButton with user feedback.

--- src/RateMovieButton.js
+++ src/RateMovieButton.js
@@ -1,21 +1,18 @@
 export default function RateMovieButton() {
-
-    const displayFeedback = () => {
-        return (
-            console.log(`Thank you for providing feedback`)
-        )
-    }
-
+    
     return (
         <>
-            <select name="rating" id="rating">
-                <option value="1"> 1/5 Stars </option>
-                <option value="2"> 2/5 Stars </option>
-                <option value="3"> 3/5 Stars </option>
-                <option value="4"> 4/5 Stars </option>
-                <option value="5"> 5/5 Stars </option>
-            </select>
-            <button type="submit" onClick={displayFeedback} >Submit Rating</button>
+    <form name="feedback-form" onSubmit={(e) => {
+      alert('Thank you for your feedback.')}}> 
+                <select name="rating" id="rating">
+                    <option value="1"> 1/5 Stars </option>
+                    <option value="2"> 2/5 Stars </option>
+                    <option value="3"> 3/5 Stars </option>
+                    <option value="4"> 4/5 Stars </option>
+                    <option value="5"> 5/5 Stars </option>
+                </select>
+                <button>Submit Rating</button>
+            </form>
         </>
     )
 }
